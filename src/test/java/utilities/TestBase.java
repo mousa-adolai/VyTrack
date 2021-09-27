b64ed576383a4647c4a7886fcc3a3acc579859b9
Mousa Adolai <mousa.adolai@gmail.com>
2021-09-27 18:40:51 -0400

    initial commit

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,24 @@
+package utilities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBase {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUpBrowser(){
+        driver = Driver.getDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+}
