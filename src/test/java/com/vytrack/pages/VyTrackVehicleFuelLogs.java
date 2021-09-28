a536009e89bc23d0db4f45949f636f1cf264517f
Rahel <rahelasgedom19@gmail.com>
2021-09-28 19:35:38 -0400

    Setting up POM for Customer Account 1

--- src/test/java/com/vytrack/pages/VyTrackVehicleFuelLogs.java
+++ src/test/java/com/vytrack/pages/VyTrackVehicleFuelLogs.java
@@ -1,11 +1,17 @@
 package com.vytrack.pages;
 
+import com.vytrack.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class VyTrackVehicleFuelLogs {
 
     @FindBy(css = "h1.oro-subtitle")
     public WebElement pageHeader;
+
+    public VyTrackVehicleFuelLogs() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
 }
--- src/test/java/com/vytrack/pages/VyTruckTest.java
+++ src/test/java/com/vytrack/pages/VyTruckTest.java
@@ -1,14 +1,18 @@
 package com.vytrack.pages;
 
+import com.vytrack.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class VyTruckTest {
 
     //h1.oro-subtitle
     @FindBy(css = "h1.oro-subtitle")
     public WebElement cutomerAcc;
-
+    public VyTruckTest() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
 
 }
