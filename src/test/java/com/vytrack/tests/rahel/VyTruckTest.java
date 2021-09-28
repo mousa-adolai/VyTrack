d690ef4d1fef4382b32999abad81050fc17e909b
Rahel <rahelasgedom19@gmail.com>
2021-09-28 19:24:32 -0400

    first trial

---
+++ src/test/java/com/vytrack/tests/rahel/VyTruckTest.java
@@ -0,0 +1,14 @@
+package com.vytrack.tests.rahel;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class VyTruckTest {
+
+    //h1.oro-subtitle
+    @FindBy(css = "h1.oro-subtitle")
+    public WebElement cutomerAcc;
+
+
+
+}
