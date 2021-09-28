ffc57285c9d9ce9b3a72f7214726066ca20faea5
Mousa Adolai <mousa.adolai@gmail.com>
2021-09-28 19:16:43 -0400

    Setting up POM for vehicle fuel logs

---
+++ src/test/java/com/vytrack/pages/VyTrackVehicleFuelLogs.java
@@ -0,0 +1,11 @@
+package com.vytrack.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class VyTrackVehicleFuelLogs {
+
+    @FindBy(css = "h1.oro-subtitle")
+    public WebElement pageHeader;
+
+}
