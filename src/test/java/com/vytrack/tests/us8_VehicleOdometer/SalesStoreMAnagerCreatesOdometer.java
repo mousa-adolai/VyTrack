669ca52b67dce59032643ed6cf08f221120173a9
Dancibela87 <dankapavlovic87@gmail.com>
2021-10-12 04:32:16 -0500

    added changes to the test case

--- src/test/java/com/vytrack/tests/us8_VehicleOdometer/SalesStoreMAnagerCreatesOdometer.java
+++ src/test/java/com/vytrack/tests/us8_VehicleOdometer/SalesStoreMAnagerCreatesOdometer.java
@@ -10,78 +10,59 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SalesStoreMAnagerCreatesOdometer extends TestBase {
+public class SalesStoreMAnagerCreatesOdometer  {
     @Test
     public void testSalesManagerCreatesOdometer(){
 
 
+            Map<String, String> credentials = new LinkedHashMap<>();
+            credentials.put("storemanager71", "UserUser123");
+            credentials.put("storemanager72", "UserUser123");
+           // credentials.put("salesmanager131", "UserUser123");
+             credentials.put("salesmanager132", "UserUser123");
+            //credentials.put("salesmanager133", "UserUser123");
 
-            VyTrackLogin loginPage = new VyTrackLogin();
-            loginPage.goTo();
-            loginPage.loginAsSalesManager();
+            for (Map.Entry<String, String> eachCredentials : credentials.entrySet()) {
+                    VyTrackLogin loginPage = new VyTrackLogin();
+                    loginPage.goTo();
+                    loginPage.login(eachCredentials.getKey(),eachCredentials.getValue());
 
-            VyTruckDashboardDriverLoggedIn dashboardElm = new VyTruckDashboardDriverLoggedIn();
-            Actions actions = new Actions(driver);
+                    VyTruckDashboardDriverLoggedIn dashboardElm = new VyTruckDashboardDriverLoggedIn();
+                    Actions actions = new Actions(Driver.getDriver());
 
-            //hovering over fleet module.
-            WebElement fleetModule1 = Driver.getDriver().findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]/a/span"));
-            actions.moveToElement(fleetModule1).perform();
+                    //hovering over fleet module.
+                    WebElement fleetModule1 = Driver.getDriver().findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]/a/span"));
+                    actions.moveToElement(fleetModule1).perform();
 
-            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+                    Driver.getDriver().manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
-            //clicking vehicle odometer from dropdown
-            WebElement vehicleOdometer = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]/div/div/ul/li[4]/a/span"));
-            vehicleOdometer.click();
+                    //clicking vehicle odometer from dropdown
+                    WebElement vehicleOdometer = Driver.getDriver().findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]/div/div/ul/li[4]/a/span"));
+                    vehicleOdometer.click();
 
-            driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+                    Driver.getDriver().manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
 
-            //no permission element
-            WebElement noPermissionMsg = Driver.getDriver().findElement(By.xpath("//*[@id=\"flash-messages\"]/div/div/div[2]/div"));
-            System.out.println(noPermissionMsg.getText());
-            String actualResult = noPermissionMsg.getText();
-            String expectedResult = "You do not have permission to perform this action.";
+                    //no permission element
+                    WebElement noPermissionMsg = Driver.getDriver().findElement(By.xpath("//*[@id=\"flash-messages\"]/div/div/div[2]/div"));
+                    System.out.println(noPermissionMsg.getText());
+                    String actualResult = noPermissionMsg.getText();
+                    String expectedResult = "You do not have permission to perform this action.";
 
-            assertEquals(expectedResult, actualResult);
+                    assertEquals(expectedResult, actualResult);
 
-        }
+                    Driver.closeBrowser();
 
-        @Test
-    public void testStoreManagerCtreatesOdometer(){
-            VyTrackLogin loginPage = new VyTrackLogin();
-            loginPage.goTo();
-            loginPage.loginAsStoreManager();
 
-            VyTruckDashboardDriverLoggedIn dashboardElm = new VyTruckDashboardDriverLoggedIn();
-            Actions actions = new Actions(driver);
+            }
 
-            //hovering over fleet module.
-            WebElement fleetModule1 = Driver.getDriver().findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]/a/span"));
-            actions.moveToElement(fleetModule1).perform();
 
-            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-
-            //clicking vehicle odometer from dropdown
-            WebElement vehicleOdometer = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]/div/div/ul/li[4]/a/span"));
-            vehicleOdometer.click();
-
-            driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-
-            //no permission element
-            WebElement noPermissionMsg = Driver.getDriver().findElement(By.xpath("//*[@id=\"flash-messages\"]/div/div/div[2]/div"));
-            System.out.println(noPermissionMsg.getText());
-            String actualResult = noPermissionMsg.getText();
-            String expectedResult = "You do not have permission to perform this action.";
-
-            assertEquals(expectedResult, actualResult);
 
         }
+
 
 }
